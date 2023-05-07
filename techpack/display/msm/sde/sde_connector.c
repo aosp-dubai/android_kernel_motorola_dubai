024f3d8ba90c14339d3b57d261edda9cf5c19546
vantoman <mustafa.vantom@gmail.com>
2023-05-07 09:34:14 +0300

    Revert "disp: msm: Implement backlight re-mapping for dubai"
    This reverts commit fa2ba37bd36d9ddf1e9a0f679ea2d01f3adda04a.

--- techpack/display/msm/sde/sde_connector.c
+++ techpack/display/msm/sde/sde_connector.c
@@ -21,8 +21,6 @@
 #include "sde_vm.h"
 #include <drm/drm_probe_helper.h>
 
-#include "bl_lut.h"
-
 #define BL_NODE_NAME_SIZE 32
 #define HDR10_PLUS_VSIF_TYPE_CODE      0x81
 
@@ -122,7 +120,7 @@ static int sde_backlight_device_update_status(struct backlight_device *bd)
 	if (brightness > c_conn->thermal_max_brightness)
 		brightness = c_conn->thermal_max_brightness;
 
-	if (brightness && brightness < display->panel->bl_config.bl_min_level)
+	if(brightness && brightness < display->panel->bl_config.bl_min_level)
 		brightness = display->panel->bl_config.bl_min_level;
 
 	/* map UI brightness into driver backlight level with rounding */
@@ -131,11 +129,6 @@ static int sde_backlight_device_update_status(struct backlight_device *bd)
 
 	if (!bl_lvl && brightness)
 		bl_lvl = 1;
-
-	if (display->panel->bl_config.bl_remap) {
-		pr_debug("%s: remap bl_lvl from %d to %d", __func__, bl_lvl, bl_lut[bl_lvl]);
-		bl_lvl = bl_lut[bl_lvl];
-	}
 
 	if (!c_conn->allow_bl_update) {
 		c_conn->unset_bl_level = bl_lvl;
@@ -173,9 +166,7 @@ done:
 
 static int sde_backlight_device_get_brightness(struct backlight_device *bd)
 {
-	struct sde_connector *c_conn = bl_get_data(bd);
-	struct dsi_display *display = (struct dsi_display *) c_conn->display;
-	return display->panel->bl_config.real_bl_level;
+	return 0;
 }
 
 static const struct backlight_ops sde_backlight_device_ops = {
